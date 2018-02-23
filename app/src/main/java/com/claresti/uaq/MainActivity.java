ea11ec9f10fb5cc66661dcd80ac5ab15a4258e49
Elias Rosales <mansion1@hotmail.com>
2018-02-22 20:53:56 -0600

    Initial commit

---
+++ app/src/main/java/com/claresti/uaq/MainActivity.java
@@ -0,0 +1,28 @@
+package com.claresti.uaq;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Adapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    Adapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Tab1"));
+        tabs.addTab(tabs.newTab().setText("Tab2"));
+        tabs.addTab(tabs.newTab().setText("Tab3"));
+
+    }
+}
---
+++ app/src/main/java/com/claresti/uaq/listContentFragment.java
@@ -0,0 +1,8 @@
+package com.claresti.uaq;
+
+/**
+ * Created by mansi on 22/02/2018.
+ */
+
+public class listContentFragment {
+}
